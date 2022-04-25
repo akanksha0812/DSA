c525ccf1a554e0acfcf8705ed761f41290f86d61
akanksha0812 <akankshamishradolly@gmail.com>
2022-04-25 23:57:26 +0530

    supchef

---
+++ codechef practice/supchef.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int m,n,x;
+        cin>>m>>n>>x;
+        if(n*x>=m){
+            cout<<"NO"<<endl;
+        }
+        else{
+            cout<<"YES"<<endl;
+        }}
+    }
