431a2daa8f4eeef8f3af8f5f4ac56e1307144c47
akanksha0812 <akankshamishradolly@gmail.com>
2022-04-27 00:27:11 +0530

    lazychf

---
+++ codechef practice/lazychf.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,m,d;
+        cin>>x>>m>>d;
+        int a=x*m;
+        int b=x+d;
+        cout<<min(a,b)<<endl;
+    }}
