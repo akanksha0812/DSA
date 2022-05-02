db640e48e54024860d518254be049d1f6e7ae4cc
akanksha0812 <akankshamishradolly@gmail.com>
2022-05-02 20:44:41 +0530

    digits_sum

---
+++ codeforces/digits_sum.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int ans=n/10;
+        if(n%10==9){
+            ans++;
+        }
+        cout<<ans<<endl;
+    }
+    return 0;
+}
