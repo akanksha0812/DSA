a853b72a1dd068a31a9e3c572912208284f8ec38
akanksha0812 <akankshamishradolly@gmail.com>
2022-05-08 21:41:50 +0530

    incriq

---
+++ codechef practice/incriq.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int q=n+7;
+    if(q>170){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+        }
+    }
+    
