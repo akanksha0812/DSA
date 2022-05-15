a75a797f40ec6df0567e75da09e10af4ba87f2bc
akanksha0812 <akankshamishradolly@gmail.com>
2022-05-15 21:24:45 +0530

    applorng

---
+++ codechef practice/lunchtime/applorng.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x;
+	cin>>x;
+	int a, b;
+	cin>>a>>b;
+	if(a*1+b*1<=x){
+	    cout<<"Yes"<<endl;
+	}
+	else{
+	    cout<<"No"<<endl;
+	}
+	return 0;
+}
