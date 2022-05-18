f8d610093b80d03a7f3b125278c4c0a18a62c521
akanksha0812 <akankshamishradolly@gmail.com>
2022-05-18 21:37:29 +0530

    C_RATING

---
+++ codechef practice/contests/C_RATING.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int t;
+	cin>>t;
+	
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    int count=0;
+	    while(x<y){
+	        count++;
+	        x+=8;
+	    }
+	    cout<<count<<endl;
+    }
+	return 0;
+}
