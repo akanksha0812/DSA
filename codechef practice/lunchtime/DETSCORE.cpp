700b35169cb80f11312adcf3e7fb4e459015df5c
akanksha0812 <akankshamishradolly@gmail.com>
2022-06-21 22:22:51 +0530

    DETSCORE

---
+++ codechef practice/lunchtime/DETSCORE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,n;
+	    cin>>x>>n;
+	    int a=x/10;
+	    cout<<a*n<<endl;;
+	}
+	return 0;
+}
