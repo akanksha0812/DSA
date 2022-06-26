114110e53a64728c690682b8f236205e24c97cd3
akanksha0812 <akankshamishradolly@gmail.com>
2022-06-26 23:56:23 +0530

    compcand

---
+++ codechef practice/compcand.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,k;
+	    cin>>n>>k;
+	    if(n%k==0){
+	        cout<<n/k<<endl;
+	    }
+	    else if(n==0){
+	        cout<<0<<endl;
+	    }
+	    else{
+	        cout<<-1<<endl;
+	    }
+	}
+	return 0;
+}
