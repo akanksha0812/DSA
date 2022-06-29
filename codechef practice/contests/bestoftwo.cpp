c4c3f0f1f8da4041bde1de33f94e209894e84411
akanksha0812 <akankshamishradolly@gmail.com>
2022-06-29 23:04:12 +0530

    bestoftwo

---
+++ codechef practice/contests/bestoftwo.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    	int x,y;
+	        cin>>x>>y;
+	        cout<<max(x,y)<<endl;
+	        
+	    
+	}
+	return 0;
+}
