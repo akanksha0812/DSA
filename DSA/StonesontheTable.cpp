4ef7ecd4398bdd140cd9867f18648667b17589fd
akanksha0812 <akankshamishradolly@gmail.com>
2022-07-02 23:36:39 +0530

    stonesOnTheTable

--- DSA/StonesontheTable.cpp
+++ DSA/StonesontheTable.cpp
@@ -1,9 +1,15 @@
 #include<iostream>
 using namespace std;
 int main(){
-    int n ;
+    int n ,count=0;
     cin>>n;
     string s;
     cin>>s;
-    
+    for(int i=0 ; i<n ; i++){
+        if(s[i]==s[i+1]){
+            count ++;
+        }
+    }
+    cout<<count<<endl;
+    return 0;
 }
--- DSA/tempCodeRunnerFile.cpp
+++ DSA/tempCodeRunnerFile.cpp
@@ -1,23 +1,3 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int l,i,n;
-    char s[10000];
-    gets(s);
-    l=strlen(s);
-    for( i=0;i<l;i++){
-        if(s[i]=='.')
-            cout<<'0';
-                if((s[i]=='-') && (s[i]=='.'))
-                {
-                    cout<<'1';
-                    i++;
-                }
-                if((s[i]=='-') && (s[i]=='-'))
-                {
-                    cout<<'2';
-                    i++;
-                }
-    }
-    cout<<endl;
-}
+ // if(s[i]=='B'|| s[i]=='G' || s[i]=='R'){
+        //     count++;
+        // }
