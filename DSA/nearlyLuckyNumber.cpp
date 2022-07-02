a82ff9ac98e6d55451530559961d91b95e06431f
akanksha0812 <akankshamishradolly@gmail.com>
2022-07-02 22:52:54 +0530

    nearlyLuckyNumber

---
+++ DSA/nearlyLuckyNumber.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    long long n,count=0;
+    cin>>n;
+    while(n!=0){
+        if(n%10==4 || n%10==7){
+            count++;
+        }
+            n=n/10;
+        }
+            if(count==4 || count==7){
+                cout<<"Yes"<<endl;
+            }
+            else{
+                cout<<"No"<<endl;
+            }
+            return 0;
+}
