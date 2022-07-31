9b1649ed11ac99a4c677b5e79cdd5b905d074be9
akanksha0812 <akankshamishradolly@gmail.com>
2022-07-31 12:12:37 +0530

    Duplicate element

--- Love Babbar/DuplicateElements.cpp
+++ Love Babbar/DuplicateElements.cpp
@@ -1,6 +1,18 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n,count=0;
+    cin >> n;
+    int arr[1000];
+    for (int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    for (int i = 0; i < n; i++){
+        for (int j = i+1; j < n; j++){
+            if (arr[i] == arr[j]){
+                cout<<arr[j];
+            }
+        }
+    }
 }
