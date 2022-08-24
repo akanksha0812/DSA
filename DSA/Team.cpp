029cb97d2c9fd84fd1160301417ef7b9de267b9e
akanksha0812 <akankshamishradolly@gmail.com>
2022-08-24 17:44:30 +0530

    Team

--- DSA/Team.cpp
+++ DSA/Team.cpp
@@ -1,5 +1,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    
+    int n,sum=0;
+    cin>>n;
+    while(n--){
+        int p,v,t;
+        cin>>p>>v>>t;
+        if((p==1&&v==1)||(v==1&&t==1)||(t==1&&p==1)){
+            sum++;
+            continue;
+        }
+    } cout<<sum<<endl;
 }
