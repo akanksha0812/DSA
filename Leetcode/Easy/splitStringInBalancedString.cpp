e89b2df748c8ef0fcec579c4f28d2eefbee6949c
akanksha0812 <akankshamishradolly@gmail.com>
2022-09-01 18:52:21 +0530

    hello world

--- Leetcode/Easy/splitStringInBalancedString.cpp
+++ Leetcode/Easy/splitStringInBalancedString.cpp
@@ -1,14 +1,5 @@
-int count=0,r=0,l=0;
-        for(int i = 0;i<s.size();i++){
-            if(s[i]=='R'){
-                r++;
-            }else if(s[i]=='L'){
-                l++;
-            }
-            if(r==l && r!=0 && l!=0){
-                count++;
-                r=0;
-                l=0;
-            }
-        }
-        return count;
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    cout<<"Hello world!";
+}
