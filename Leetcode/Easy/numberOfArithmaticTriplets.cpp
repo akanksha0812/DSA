f0704a84a928112ac47c25e55eb713408fec1d5a
akanksha0812 <akankshamishradolly@gmail.com>
2022-09-03 23:16:06 +0530

    number of arithmatic triplets

---
+++ Leetcode/Easy/numberOfArithmaticTriplets.cpp
@@ -0,0 +1,11 @@
+int count=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                for(int k=j+1;k<nums.size();k++){
+                    if(nums[j]-nums[i]==diff && nums[k]-nums[j]==diff){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
