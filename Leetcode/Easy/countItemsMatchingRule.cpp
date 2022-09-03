ebe78daf2319a1a4020f01476f3544e25a1f29e1
akanksha0812 <akankshamishradolly@gmail.com>
2022-09-03 23:17:51 +0530

    count items matching the rule

---
+++ Leetcode/Easy/countItemsMatchingRule.cpp
@@ -0,0 +1,13 @@
+ int count=0;
+        for(int i=0;i<items.size();i++){
+            if(ruleKey=="type" && items[i][0]== ruleValue){
+               count++; 
+            }
+            else if(ruleKey=="color" && items [i][1]==ruleValue){
+                count++;
+            }
+            else if(ruleKey=="name" && items[i][2]==ruleValue){
+                count++;
+            }  
+        }
+        return count; 
