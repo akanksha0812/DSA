c2a87b20d4c384bd3eb84fd2a02b78b03defc959
akanksha0812 <akankshamishradolly@gmail.com>
2022-09-08 23:53:21 +0530

    check if two string arrays are equivalent

---
+++ Leetcode/Easy/checkIfTwoStringArraysAreEquivalent.cpp
@@ -0,0 +1,9 @@
+ string a="", b="";
+                
+          for(int i=0;i<word1.size();i++){
+              a+=word1[i];
+              }
+        for(int j=0;j<word2.size();j++){
+              b+=word2[j];
+              }
+        return a==b;
