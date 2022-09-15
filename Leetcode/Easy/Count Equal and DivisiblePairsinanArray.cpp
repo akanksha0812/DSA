b66350bef8ebd224541b889a93e4293a366b8776
akanksha0812 <akankshamishradolly@gmail.com>
2022-09-15 21:44:46 +0530

    Count Equal and Divisible Pairs in an Array

---
+++ Leetcode/Easy/Count Equal and DivisiblePairsinanArray.cpp
@@ -0,0 +1,9 @@
+int count=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]==nums[j] && (i*j)%k==0){
+                    count++;
+                }
+            }
+        }
+        return count;
