b10731d022985e181b58d1de50a5d8fd6fb4363d
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-05-28 21:57:45 +0530

    Update Nth node from end of linked list.cpp

--- gfg/ptod/Nth node from end of linked list.cpp
+++ gfg/ptod/Nth node from end of linked list.cpp
@@ -3,22 +3,20 @@ public:
     int getNthFromLast(Node *head, int n)
     {
            // Your code here
-           Node *p1=head;
-    	Node *p2=head;
-    	
-    	//placing 2nd pointer
-    	int c=0;
-    	for(int i=n;i>0&&p2!=NULL;i--){
-    	    c++;
-    	    p2=p2->next;
-    	   
-    	}
-    	 if(p2==NULL&&c!=n)return -1;
-    	//finding the ans value
-    	while(p2!=NULL){
-    	    p1=p1->next;
-    	    p2=p2->next;
-    	}
-    	return p1->data;
+           Node*temp= head;
+          int c=0;
+          while(temp!=0)
+          {
+              c++;
+              temp=temp->next;
+          }
+          temp=head;
+          if(n>c)
+          return -1;
+          for(int i=0;i<(c-n);i++)
+          {
+              temp=temp->next;
+          }
+          return temp->data;
     }
 };
