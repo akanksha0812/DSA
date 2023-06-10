5e73f6c386fc7a8020cc0e8470f79ef9bc87c7b7
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-06-10 23:22:09 +0530

    Create Arranging the array.cpp

---
+++ gfg/ptod/Arranging the array.cpp
@@ -0,0 +1,24 @@
+class Solution
+{
+    public:
+        void Rearrange(int arr[], int n)
+        {
+            // Your code goes here
+            vector<int> neg;
+            vector<int> pos;
+            for(int i=0;i<n;i++){
+                if(arr[i]<0){
+                    neg.push_back(arr[i]);
+                }
+                else{
+                    pos.push_back(arr[i]);
+                }
+            }
+            for(int i=0;i<neg.size();i++){
+                arr[i]=neg[i];
+            }
+            for(int i=0;i<pos.size();i++){
+                arr[i+neg.size()]=pos[i];
+            }
+        }
+};
