735beef5ee1e27808becd93a6a2a33671e03904b
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-07-02 21:26:30 +0530

    Create Copy Set Bits in Range.cpp

---
+++ gfg/ptod/Copy Set Bits in Range.cpp
@@ -0,0 +1,12 @@
+class Solution{
+    public:
+    int setSetBit(int x, int y, int l, int r){
+        // code here
+        for(int i=l-1; i<r; i++) {
+        if((y&(1<<i)) == (1<<i)) {
+            x|=(1<<i);
+        }
+    }
+    return x;
+    }
+};
