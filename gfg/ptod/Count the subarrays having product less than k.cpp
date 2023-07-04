0662b011c46be985909c60d723714f92fb1faf7b
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-07-04 23:10:41 +0530

    Create Count the subarrays having product less than k.cpp

---
+++ gfg/ptod/Count the subarrays having product less than k.cpp
@@ -0,0 +1,16 @@
+class Solution{
+  public:
+    int countSubArrayProductLessThanK(const vector<int>& a, int n, long long k) {
+        long long i=0,j=0,cnt=0,pro=1,ans=0;
+        while(i<n){
+            pro*=a[i];
+            while(j<=i && pro>=k){
+                pro/=a[j];
+                j++;
+            }
+            ans+=(i-j+1);
+            i++;
+        }
+        return ans;
+    }
+};
