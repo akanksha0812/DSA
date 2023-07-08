a0500eafe4cdfa48bfa2684c6244fc923ed6ac7e
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-07-08 22:28:30 +0530

    Create Put Marbles in Bags.cpp

---
+++ Leetcode/problem_of_the_day/Put Marbles in Bags.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        long long minans = weights[0] + weights[weights.size()-1];
+        long long maxans = minans;
+        priority_queue<int> pqmax;
+        priority_queue<int, vector<int>, greater<int>> pqmin;
+        for(int i=1;i<weights.size();i++)
+        {
+            pqmin.push(weights[i] + weights[i-1]);
+            pqmax.push(weights[i] + weights[i-1]);
+        } 
+        for(int i=1;i<k;i++)
+        {
+            minans += pqmin.top();
+            maxans += pqmax.top();
+            pqmin.pop();
+            pqmax.pop();
+        }
+        return (maxans - minans);
+    }
+};
