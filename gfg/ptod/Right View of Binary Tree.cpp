8ba76b1a1e5bddfa5ba0b619173fe1cd5fd4c12c
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-07-24 21:22:48 +0530

    Create Right View of Binary Tree.cpp

---
+++ gfg/ptod/Right View of Binary Tree.cpp
@@ -0,0 +1,29 @@
+class Solution
+{
+    public:
+    //Function to return list containing elements of right view of binary tree.
+    vector<int> rightView(Node *root)
+    {
+       // Your Code here
+        vector<int>ans;
+        queue<Node*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            Node* node=q.front();
+            int n=q.size();
+            ans.push_back(node->data);
+            while(n--)
+            {
+                Node* temp=q.front();
+                q.pop();
+                if(temp->right)
+                  q.push(temp->right);
+                if(temp->left)
+                  q.push(temp->left);
+                
+            }
+        }
+        return ans;
+    }
+};
