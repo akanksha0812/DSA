ae8ceb288045d41f717e6d073cac3286183f5808
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-08-27 21:39:02 +0530

    Create Reverse a String.cpp

---
+++ gfg/ptod/Reverse a String.cpp
@@ -0,0 +1,11 @@
+//User function Template for C++
+class Solution
+{
+    public:
+    string reverseWord(string str)
+    {
+        // Your code goes here
+        reverse(str.begin(),str.end());
+        return str;
+    }
+};
