42a8ddc6db7cda0f93cb6714f36b17d54cb4e1a0
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-09-05 23:13:13 +0530

    Create class_Implimenationof_bfs.cpp

---
+++ gfg/Graph/class_Implimenationof_bfs.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        vector<int> bfsofGraph(int v, vector<int> adj[]){
+            int vis[v]={0};
+            vis[0]=1;
+            queue<int> q;
+            q.push(0);
+            vector<int> bfs;
+            while(!q.empty()){
+                int node =q.front();
+                q.pop();
+                bfs.push_back(node);
+                for(auto it: adj[node]){
+                    if(!vis[it]){
+                        vis[it]=1;
+                        q.push(it);
+                    }
+                }
+            }
+            return bfs;
+        }
+};
+
+
+
+void addEdge(vector<int> adj[],int u,int v){
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+}
+
+void printAns(vector<int> &ans){
+    for(int i=0;i<ans.size();i++){
+        cout<< ans[i]<<" ";
+    }
+}
+
+int main(){
+    int n;
+    cout<<"Enter no of nodes"<<endl;
+    cin>>n;
+    vector<int> adj[n];
+    int m;
+    cout<<"Enter the no of edges"<<endl;
+    cin>>m;
+    int u,v;
+    cout<<"Enter the edges(u,v)"<<endl;
+    for(int i=0;i<m;i++){
+        cin>>u>>v;
+        addEdge(adj,u,v);
+    }
+    // vector<int> adj[6];
+    // addEdge(adj,0,1);
+    // addEdge(adj,1,2);
+    // addEdge(adj,1,3);
+    // addEdge(adj,0,4);
+
+    Solution obj;
+    vector<int> ans=obj.bfsofGraph(5,adj);
+
+    printAns(ans);
+    return 0;
+}
