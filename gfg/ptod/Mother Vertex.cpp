63cd42621c4512655607cd289bca155e9f743508
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-09-06 22:35:31 +0530

    Create Mother Vertex.cpp

---
+++ gfg/ptod/Mother Vertex.cpp
@@ -0,0 +1,52 @@
+
+class Solution 
+{
+     private:
+ 
+ void dfs(int vertex , vector<int>adj[] , vector<bool>&visited , stack<int>&stk)
+ {
+    visited[vertex] = true;
+    for(auto node : adj[vertex])
+    {
+        if(!visited[node])
+        {
+            dfs(node,adj,visited,stk);
+        }
+    }
+    stk.push(vertex);
+ }
+
+
+ bool allVisited(vector<bool>&vis)
+ {
+    for(int i=0;i<vis.size();i++)
+    {
+        if(vis[i] == false) return false;
+    }
+    return true;
+ }
+    public:
+    //Function to find a Mother Vertex in the Graph.
+	int findMotherVertex(int V, vector<int>adj[])
+	{
+	    // Code here
+	    vector<bool>vis(V,false);
+        stack<int>stk;
+        for(int i=0;i<V;i++)
+        {
+            if(!vis[i])
+                dfs(i,adj,vis,stk);
+        }
+        
+        int top = stk.top();
+        
+        for(int i=0;i<V;i++) vis[i] = false;
+        
+        dfs(top,adj,vis,stk);
+        
+        if(allVisited(vis)) return top;
+        
+        return -1;
+	}
+
+};
