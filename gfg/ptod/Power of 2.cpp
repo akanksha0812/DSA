9ef434341cf22731f3983fd14b6319b59973180e
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-09-18 23:22:13 +0530

    Create Power of 2.cpp

---
+++ gfg/ptod/Power of 2.cpp
@@ -0,0 +1,11 @@
+//User function Template for C++
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here    
+         return log2(n)==(long long int)log2(n);
+    }
+};
