809f47c4947463e3d8f56a30870c65ed38f6cfad
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-10-20 20:32:17 +0530

    Create Flatten Nested List Iterator.cpp

---
+++ gfg/ptod/Flatten Nested List Iterator.cpp
@@ -0,0 +1,16 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int isPossible(int N, int arr[]) {
+        // code here
+        long sum=0;
+        for(int i=0;i<N;i++){
+            sum+=arr[i];
+        }
+        if(sum%3==0){
+            return 1;
+        }
+        return 0;
+    }
+};
