097780603cb62efc646fad18abb6ebb2b9403ec5
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-10-21 23:47:47 +0530

    Create Sum of all divisors from 1 to n.cpp

---
+++ gfg/ptod/Sum of all divisors from 1 to n.cpp
@@ -0,0 +1,16 @@
+//User function Template for C++
+class Solution
+{
+public:
+    long long sumOfDivisors(int N)
+    {
+        // Write Your Code here
+        long long ans=0;
+        
+        for(int i=1;i<=N;i++){
+            ans+= (i*(N/i));
+        }
+        
+        return ans;
+    }
+};
