e670b860c2563c8fe934a9defa987e4a96e972ba
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-10-29 22:36:07 +0530

    Create Check whether K-th bit is set or not.cpp

---
+++ gfg/ptod/Check whether K-th bit is set or not.cpp
@@ -0,0 +1,14 @@
+//User function Template for C++
+
+
+class Solution
+{
+    public:
+    // Function to check if Kth bit is set or not.
+    bool checkKthBit(int n, int k)
+    {
+        // Your code here
+        // It can be a one liner logic!! Think of it!!
+        return (n>>k)&1;
+    }
+};
