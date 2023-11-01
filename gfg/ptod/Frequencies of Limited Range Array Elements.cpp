47925e9276ef1e13d339e7f8399810cd38f9a377
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-11-01 22:45:06 +0530

    Create Frequencies of Limited Range Array Elements.cpp

---
+++ gfg/ptod/Frequencies of Limited Range Array Elements.cpp
@@ -0,0 +1,17 @@
+class Solution{
+    public:
+    //Function to count the frequency of all elements from 1 to N in the array.
+    void frequencyCount(vector<int>& arr,int N, int P)
+    { 
+        // code here
+        unordered_map<int,int> m;
+        for(int i=0;i<N;i++)
+        {
+            m[arr[i]]++;
+        }
+        for(int i=0;i<N;i++)
+        {
+            arr[i]=m[i+1];
+        }
+    }
+};
