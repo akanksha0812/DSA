f0094cf8f93280cfd381d76f5d2d849fc72887ef
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-11-17 23:01:07 +0530

    Create Binary Tree to CDLL.cpp

---
+++ gfg/ptod/Binary Tree to CDLL.cpp
@@ -0,0 +1,47 @@
+/*Complete the function below
+Node is as follows:
+struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+    
+    Node(int x){
+        data = x;
+        left = right = NULL;
+    }
+};*/
+
+class Solution
+{
+  public:
+    //Function to convert binary tree into circular doubly linked list.
+    void solve(Node* root , Node* &head){
+        
+        if(root == NULL){
+            return;
+        }
+        
+        solve(root->right,head);
+        root->right = head;
+        if(head != NULL){
+            head->left = root;
+        }
+        head = root;
+        solve(root->left,head);
+        
+    }
+    
+    Node *bTreeToCList(Node *root)
+    {
+        Node* head = NULL;
+        solve(root,head);
+        Node* tail = head;
+        while(tail->right != NULL){
+            tail = tail->right;
+        }
+        tail->right = head;
+        head->left = tail;
+        return head;
+    }
+};
