55605b10f60920a3c1c9f3d1bc3071f8546ed910
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-11-20 23:38:30 +0530

    Create K Sum Paths.cpp

---
+++ gfg/ptod/K Sum Paths.cpp
@@ -0,0 +1,50 @@
+//User function template for C++
+
+/*
+struct Node 
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+class Solution{
+  public:
+    
+    void help(Node* root,int& ans,int& sum,int k,unordered_map<int,int>& mp)
+    {
+        //base case
+        if(!root)
+        return ;
+        
+        //small calculation
+        //recursive calls
+        sum+=root->data;
+        if(mp.count(sum-k))
+        ans+=mp[sum-k];
+        
+        if(sum==k)
+        ans++;
+        
+        mp[sum]++;
+        help(root->left,ans,sum,k,mp);
+        help(root->right,ans,sum,k,mp);
+        mp[sum]--;
+        sum-=root->data;
+        
+    }
+    
+    int sumK(Node *root,int k)
+    {
+        unordered_map<int,int> mp;
+        int sum=0;
+        int ans=0;
+        help(root,ans,sum,k,mp);
+        return ans;
+    }
+};
