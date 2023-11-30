1f0f3eb98f525d0bb875b5021f00bed7ec53d177
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-11-30 22:43:35 +0530

    Create Shortest path from 1 to n.cpp

---
+++ gfg/ptod/Shortest path from 1 to n.cpp
@@ -0,0 +1,15 @@
+//User function Template for C++
+
+class Solution{   
+public:
+    int minimumStep(int n){
+        //complete the function here
+           int ans = 0;
+        while(n >= 3){
+            ans += n % 3 + 1;
+            n /= 3;
+        }
+
+        return ans + (n - 1);
+    }
+};
