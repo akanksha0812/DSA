cdea3d50514611052f272ba80965cc232dc02d10
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-12-20 22:47:40 +0530

    Create Modified Game of Nim.cpp

---
+++ gfg/ptod/Modified Game of Nim.cpp
@@ -0,0 +1,15 @@
+// User function Template for C++
+
+class Solution{
+public:
+    int findWinner(int n, int A[]){
+        // code here
+        int res = 0 ;
+        for(int idx = 0 ; idx < n ; idx++){
+            res ^= A[idx];
+        }
+        if(res == 0)
+         return 1;
+         return (n & 1)?2:1;
+    }
+};
