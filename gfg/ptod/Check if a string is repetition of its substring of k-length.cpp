e17724bd6cd01fe8ceb4fe2ace29b72d7edf024c
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2023-12-29 21:45:01 +0530

    Create Check if a string is repetition of its substring of k-length.cpp

---
+++ gfg/ptod/Check if a string is repetition of its substring of k-length.cpp
@@ -0,0 +1,31 @@
+//User function template for C++
+class Solution{
+public:
+	
+	int kSubstrConcat (int n, string s, int k)
+	{
+	 if(n%k) return 0;
+        map<string,int>mp;
+        string temp = "";
+        temp += s[0];
+        for(int i=1;i<n;i++) {
+            if(i%k==0){
+                mp[temp]++;
+                temp = "";
+            }
+            temp += s[i];
+        }
+        mp[temp]++;
+        if(mp.size()>2) return 0;
+        else{
+            int count=0;
+            for(auto it:mp) {
+                if(it.second>1) {
+                    count++;
+                }
+            }
+            if(count>1) return 0;
+            return 1;
+        }
+	}
+};
