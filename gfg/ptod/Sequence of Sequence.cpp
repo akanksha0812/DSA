985bbf5e6eadf65f8fbdf9ed6c550cfa20513f17
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-01-16 23:05:35 +0530

    Create  Sequence of Sequence.cpp

---
+++ gfg/ptod/Sequence of Sequence.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+class Solution{
+public:
+    int numberSequence(int m, int n){
+        // code here
+        if(m==0||n==1)return m;
+        int ans = 0;
+        for(int i = m;i>=1;i--){
+            ans += numberSequence(i/2,n-1);
+        }
+        return ans;
+    }
+};
