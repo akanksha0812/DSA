1472e6bff6fa1341a6f88a960199742891301175
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-02-07 21:12:21 +0530

    Create Min distance between two given nodes of a Binary Tree.cpp

---
+++ gfg/ptod/Min distance between two given nodes of a Binary Tree.cpp
@@ -0,0 +1,59 @@
+/* A binary tree node
+struct Node
+{
+    int data;
+    Node* left, * right;
+}; */
+
+class Solution{
+    public:
+    /* Should return minimum distance between a and b
+    in a tree with given root*/
+    Node * LCA(Node * root, int a, int b){
+        if(root == NULL || root->data == a || root->data == b){
+            return root;
+        }
+        
+        Node * left = LCA(root->left,a,b);
+        Node * right = LCA(root->right,a,b);
+        
+        if(left == NULL){
+            return right;
+        }
+        else if(right == NULL){
+            return left;
+        }
+        else{
+            return root;
+        }
+    }
+    
+    int dist(Node * root, int a,int d){
+        
+        if(root == NULL){
+            return 0;
+        }
+        
+        if(root->data == a){
+            return d;
+        }
+        
+        int left = dist(root->left,a,d+1);
+        int right = dist(root->right,a,d+1);
+        
+        return max(left,right);
+    }
+    /* Should return minimum distance between a and b
+    in a tree with given root*/
+    int findDist(Node* root, int a, int b) {
+        
+        
+        Node* lca = LCA(root,a,b);
+        
+        int dista = dist(lca,a,0);
+        int distb = dist(lca,b,0);
+        
+        return dista+distb;
+        // Your code here
+    }
+};
