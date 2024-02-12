87c3373ef2094396707ea7e49a777cda51779c13
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-02-12 19:08:48 +0530

    Create Recursive sequence.cpp

---
+++ gfg/ptod/Recursive sequence.cpp
@@ -0,0 +1,19 @@
+//User function Template for C++
+
+class Solution{
+public:
+    long long sequence(int n){
+        // code here
+        long long mod = 1e9 + 7;
+        long long out = 0, c = 1;
+        for(int i = 1; i <= n; ++i){
+            long long temp = 1;
+            for(int j = 0; j < i ; ++j){
+                temp *= c++;
+                temp %= mod;
+            }
+            out = (out + temp) % mod;
+        }
+        return out;
+    }
+};
