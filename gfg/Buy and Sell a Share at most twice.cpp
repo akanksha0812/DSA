b49a60f4209417173874a607103596a16e59188d
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-02-23 23:28:29 +0530

    Create Buy and Sell a Share at most twice.cpp

---
+++ gfg/Buy and Sell a Share at most twice.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+    public:
+        int maxProfit(vector<int>&price){
+            int n = price.size();
+            vector<int> mx(n, 0);
+            int ans = 0;
+            for (int k = 0; k < 4; ++k) {
+                int last = mx[k];
+                mx[k] = (k ? mx[k-1] : 0) + (k&1 ? price[k] : -price[k]);
+                for (int i = k+1; i < n; ++i) {
+                    int tmp = last + (k&1 ? price[i] : -price[i]);
+                    last = mx[i];
+                    mx[i] = max(tmp, mx[i-1]); 
+                }
+                ans = max(mx[n-1], ans);
+            }
+            return ans;
+        }
+};
