d336c2273be7fea3ec830b287bf7b5135913a787
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-03-01 22:47:02 +0530

    Create 2864. Maximum Odd Binary Number.cpp

---
+++ Leetcode/problem_of_the_day/2864. Maximum Odd Binary Number.cpp
@@ -0,0 +1,19 @@
+class Solution 
+{
+public:
+    string maximumOddBinaryNumber(string s) 
+    {
+        sort(s.begin(),s.end(), greater<int>());
+
+        for(int i = s.size()-1; i>=0; i--)
+        {
+            if(s[i] == '1')
+            {
+                swap(s[i],s[s.size()-1]);
+                break;
+            }
+        }
+
+        return s;
+    }
+};
