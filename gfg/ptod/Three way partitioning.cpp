ced4c31b0dc40ae597a2149ac06c4e50bf7f7ddc
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-04-21 22:36:32 +0530

    Create Three way partitioning.cpp

---
+++ gfg/ptod/Three way partitioning.cpp
@@ -0,0 +1,20 @@
+class Solution{   
+public:
+    void threeWayPartition(vector<int>& array,int a, int b)
+    {
+        int n = array.size();
+        int low = 0, mid = 0, high = n - 1;
+        while (mid <= high) {
+            if (array[mid] < a) {
+                swap(array[low], array[mid]);
+                low++;
+                mid++;
+            } else if (array[mid] > b) {
+                swap(array[mid], array[high]);
+                high--;
+            } else {
+                mid++;
+            }
+        }
+    }
+};
