e60b94e7e8babc9e8ea77c149a5b264f887704fb
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-05-11 21:42:04 +0530

    Create Juggler Sequence.cpp

---
+++ gfg/ptod/Juggler Sequence.cpp
@@ -0,0 +1,47 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    void solve(long long n,vector<long long> &a){
+        if(n==1){
+           // a.push_back(n);
+            return;
+        }
+        
+        if(n%2==0){
+            n=sqrt(n);
+        }
+        else //if(n%2!=0)
+        {
+            n=(sqrt(n))*n;
+        }
+        a.push_back(n);
+        
+        solve(n,a);
+        return;
+    }
+  
+    vector<long long> jugglerSequence(long long n) {
+        // code here
+        vector<long long> ans;
+        ans.push_back(n);
+        
+        //solve(n,ans);// --->using Recursions...
+        
+
+       //iterative Approach....
+        while(n!=1){
+            if(n%2==0){
+                n=sqrt(n);
+            }
+            else
+            {
+                n=n*sqrt(n);
+            }
+            
+            ans.push_back(n);
+        }
+        
+        return ans;
+    }
+};
