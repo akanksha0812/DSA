e0856fd72e5a4d27b25a1867676147418e0d53e7
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-05-12 22:16:47 +0530

    Create Minimum steps to destination.cpp

---
+++ gfg/ptod/Minimum steps to destination.cpp
@@ -0,0 +1,22 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int minSteps(int d) {
+        // code here
+        int add = 1;
+        int element = 0, count = 0;
+        while(element<d){
+            element+=add;
+            add++;
+            count++;
+        }
+        
+        while((element-d)%2 != 0){
+            count++;
+            element+=add;
+            add++;
+        }
+        return count;
+    }
+};
