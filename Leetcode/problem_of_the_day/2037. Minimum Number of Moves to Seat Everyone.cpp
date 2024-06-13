fb3ac90d0ff08b9a4ed405c438ceeec4be32d67b
Akanksha Mishra <90760374+akanksha0812@users.noreply.github.com>
2024-06-13 20:32:34 +0530

    Create 2037. Minimum Number of Moves to Seat Everyone.cpp

---
+++ Leetcode/problem_of_the_day/2037. Minimum Number of Moves to Seat Everyone.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+         int ans=0;
+        sort(seats.begin(),seats.end());
+        sort(students.begin(),students.end());
+        for(int i=0;i<seats.size();i++){
+            if(seats[i]==students[i]){
+                continue;
+            }
+            else{
+                ans=ans+abs(seats[i]-students[i]);
+            }
+        }
+        return ans;
+    }
+};
